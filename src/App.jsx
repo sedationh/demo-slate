d17323e2d3f7cf87b74808111af57b6492f40c6d
SedationH <sedationh@gmail.com>
2023-09-23 17:14:18 +0800

    pre Executing Commands

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
+import { useState, useCallback } from "react"
+import { createEditor, Transforms, Element, Editor } from "slate"
+import { Slate, Editable, withReact } from "slate-react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialValue = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+]
+
+const CodeElement = (props) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
+const DefaultElement = (props) => {
+  return <p {...props.attributes}>{props.children}</p>
+}
+
+const Leaf = (props) => {
+  return (
+    <span {...props.attributes} style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}>
+      {props.children}
+    </span>
+  )
+}
+
+const App = () => {
+  const [editor] = useState(() => withReact(createEditor()))
+
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    // Add the editable component inside the context.
+    <Slate editor={editor} initialValue={initialValue}>
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={Leaf}
+        onKeyDown={(event) => {
+          if (event.key === "&") {
+            event.preventDefault()
+            editor.insertText("and")
+          }
+
+          if (event.key === "`" && event.ctrlKey) {
+            event.preventDefault()
+            // Determine whether any of the currently selected blocks are code blocks.
+            const [match] = Editor.nodes(editor, {
+              match: (n) => n.type === "code",
+            })
+            // Toggle the block type depending on whether there's already a match.
+            Transforms.setNodes(
+              editor,
+              { type: match ? "paragraph" : "code" },
+              { match: (n) => Element.isElement(n) && Editor.isBlock(editor, n) }
+            )
+          }
+
+          if (event.key === "b" && event.ctrlKey) {
+            event.preventDefault()
+            Editor.addMark(editor, "bold", true)
+          }
+        }}
+      />
+    </Slate>
   )
 }
 
