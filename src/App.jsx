cf3bf3313a948844ac00039055a95fc698227926
SedationH <sedationh@gmail.com>
2023-09-23 17:35:33 +0800

    over Saving to a Database

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 /* eslint-disable react/prop-types */
-import { useState, useCallback } from "react"
-import { createEditor, Transforms, Element, Editor } from "slate"
+import { useState, useCallback, useMemo } from "react"
+import { createEditor, Transforms, Editor, Element } from "slate"
 import { Slate, Editable, withReact } from "slate-react"
-
-const initialValue = [
-  {
-    type: "paragraph",
-    children: [{ text: "A line of text in a paragraph." }],
-  },
-]
 
 const CodeElement = (props) => {
   return (
@@ -30,8 +23,53 @@ const Leaf = (props) => {
   )
 }
 
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const marks = Editor.marks(editor)
+    return marks ? marks.bold === true : false
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.type === "code",
+    })
+
+    return !!match
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor)
+    if (isActive) {
+      Editor.removeMark(editor, "bold")
+    } else {
+      Editor.addMark(editor, "bold", true)
+    }
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor)
+    // Toggle the block type depending on whether there's already a match.
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? "paragraph" : "code" },
+      { match: (n) => Element.isElement(n) && Editor.isBlock(editor, n) }
+    )
+  },
+}
+
 const App = () => {
   const [editor] = useState(() => withReact(createEditor()))
+
+  const initialValue = useMemo(
+    () =>
+      JSON.parse(localStorage.getItem("content")) || [
+        {
+          type: "paragraph",
+          children: [{ text: "A line of text in a paragraph." }],
+        },
+      ],
+    []
+  )
 
   const renderElement = useCallback((props) => {
     switch (props.element.type) {
@@ -44,7 +82,18 @@ const App = () => {
 
   return (
     // Add the editable component inside the context.
-    <Slate editor={editor} initialValue={initialValue}>
+    <Slate
+      editor={editor}
+      initialValue={initialValue}
+      onChange={(value) => {
+        const isAstChange = editor.operations.some((op) => "set_selection" !== op.type)
+        if (isAstChange) {
+          // Save the value to Local Storage.
+          const content = JSON.stringify(value)
+          localStorage.setItem("content", content)
+        }
+      }}
+    >
       <Editable
         renderElement={renderElement}
         renderLeaf={Leaf}
@@ -56,21 +105,13 @@ const App = () => {
 
           if (event.key === "`" && event.ctrlKey) {
             event.preventDefault()
-            // Determine whether any of the currently selected blocks are code blocks.
-            const [match] = Editor.nodes(editor, {
-              match: (n) => n.type === "code",
-            })
-            // Toggle the block type depending on whether there's already a match.
-            Transforms.setNodes(
-              editor,
-              { type: match ? "paragraph" : "code" },
-              { match: (n) => Element.isElement(n) && Editor.isBlock(editor, n) }
-            )
+
+            CustomEditor.toggleCodeBlock(editor)
           }
 
           if (event.key === "b" && event.ctrlKey) {
             event.preventDefault()
-            Editor.addMark(editor, "bold", true)
+            CustomEditor.toggleBoldMark(editor)
           }
         }}
       />
